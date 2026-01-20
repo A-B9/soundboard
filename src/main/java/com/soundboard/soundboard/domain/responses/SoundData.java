a5873719fc6d6874d201ab6413391595a5431729
arbnorbregu <arbnor.bregu@gmail.com>
2026-01-20 17:33:05 +0000

    models

---
+++ src/main/java/com/soundboard/soundboard/domain/responses/SoundData.java
@@ -0,0 +1,13 @@
+//package com.soundboard.soundboard.domain.responses;
+//
+//import com.soundboard.soundboard.domain.models.SoundEntity;
+//
+//public record SoundData(
+//        String name,
+//        char keyBinding
+//) {
+//
+//  public static SoundData createData(SoundEntity sound) {
+//    return new SoundData(sound.getName(), sound.getKeyBinding());
+//  }
+//}
