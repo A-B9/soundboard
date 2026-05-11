612525be0e6604752842c520f5fd254d3a0c746b
arbnorbregu <arbnor.bregu@gmail.com>
2026-05-11 10:24:59 +0100

    bash script improvement for updating soundboard

--- scripts/update-soundboard.sh
+++ scripts/update-soundboard.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "Rebuilding docker image"
-docker compose up -d --build --no-deps soundboard
+docker compose build soundboard
+docker compose up -d --no-deps soundboard
 
 echo "Soundboard application updated! The Soundboard application should now be running with the latest changes."
