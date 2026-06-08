57088accf7a091045cf01973714848b00db9bbb2
arbnorbregu <arbnor.bregu@gmail.com>
2026-06-08 21:04:57 +0100

    set size restriction on inputs

--- src/main/java/com/soundboard/soundboard/models/requestModels/PatchSoundRequest.java
+++ src/main/java/com/soundboard/soundboard/models/requestModels/PatchSoundRequest.java
@@ -8,10 +8,12 @@ import java.util.List;
 public record PatchSoundRequest(
         @Size(min = 1, message = "Name must not be blank if provided")
         String name,
-
+        
+        @Size(min = 1, max = 250)
         String description,
 
         SoundCategoryEnum category,
 
+        @Size(min = 1, max = 25)
         List<String> tags
 ) {}
